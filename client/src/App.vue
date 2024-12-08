a4024fbc964136f6c1acfeb08bbd8bf8893f87c8
rguixaro <rguixaro@protonmail.ch>
2024-12-08 23:02:38 +0100

    feat(style): add styling & assets

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+import Sidebar from '@/components/layout/sidebar/Sidebar.vue'
+import Header from '@/components/layout/header/Header.vue'
+</script>
+
+<template>
+  <div class="flex select-none">
+    <Header />
+    <Sidebar />
+    <router-view />
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: 'atkinson';
+  src: url('./assets/fonts/atkinson-regular.woff');
+}
+@font-face {
+  font-family: 'atkinson-bold';
+  src: url('./assets/fonts/atkinson-bold.woff');
+}
+</style>
