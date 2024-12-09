ad6da6f236abacb162c4541985f9a28e0a710eca
rguixaro <rguixaro@protonmail.ch>
2024-12-09 14:03:56 +0100

    feat(testing): add tests

--- client/src/services/api/client.ts
+++ client/src/services/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const TIMEOUT = 5000
-const API_URL = 'http://localhost:3000/'
+const API_URL = 'http://localhost:3000/api/v1/'
 
 /**
  * Axios instance used to connect to the API
