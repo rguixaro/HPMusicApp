fe686ed2ae816290cf48fff9693882ffb14308d2
rguixaro <rguixaro@protonmail.ch>
2024-12-10 17:44:09 +0100

    fix(api): add pro api setup

--- client/src/services/api/client.ts
+++ client/src/services/api/client.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
+const { DEV } = import.meta.env
 
 const TIMEOUT = 5000
-const API_URL = 'http://localhost:3000/api/v1/'
+const HOST = DEV ? 'http://localhost:3000' : 'https://api.music.rguixaro.dev'
 
 /**
  * Axios instance used to connect to the API
  */
-const instance = axios.create({ baseURL: API_URL, timeout: TIMEOUT })
+const instance = axios.create({ baseURL: `${HOST}/api/v1/`, timeout: TIMEOUT })
 
 export default instance
